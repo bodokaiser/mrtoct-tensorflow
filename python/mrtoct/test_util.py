0417fb143a08765636a6cf96bca354e1995bcd72
Bodo Kaiser <i@bodokaiser.io>
2017-09-25 16:01:48 +0200

    updated and added tests for mrtoct.util

--- python/mrtoct/test_util.py
+++ python/mrtoct/test_util.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 from mrtoct import util
@@ -5,28 +6,42 @@ from mrtoct import util
 
 class UtilTest(tf.test.TestCase):
 
-  def testMeshgrid(self):
+  def test_meshgrid_2d(self):
+    x = util.meshgrid_2d([0, 0], [2, 2])
+    y = util.meshgrid_2d([10, 10], [14, 14], 2)
+
+    with self.assertRaises(ValueError):
+      util.meshgrid_2d([0], [1, 2])
+    with self.assertRaises(ValueError):
+      util.meshgrid_2d([1], [1])
+    with self.assertRaises(ValueError):
+      util.meshgrid_2d([1, 2, 3, 4], [1, 2, 3, 4])
+
     with self.test_session():
-      x = util.meshgrid([0, 0], [2, 2])
-      y = util.meshgrid([10, 10], [14, 14], 2)
-      z = util.meshgrid([0, 0, 0], [1, 2, 2])
-
-      with self.assertRaises(ValueError):
-        util.meshgrid([0], [1, 2])
-      with self.assertRaises(ValueError):
-        util.meshgrid([1], [1])
-      with self.assertRaises(ValueError):
-        util.meshgrid([1, 2, 3, 4], [1, 2, 3, 4])
-
       self.assertAllEqual([[[0, 0], [0, 1]],
                            [[1, 0], [1, 1]]], x.eval())
       self.assertAllEqual([[[10, 10], [10, 12]],
                            [[12, 10], [12, 12]]], y.eval())
-      self.assertAllEqual(
-          [[
-              [[0, 0, 0], [0, 0, 1]],
-              [[0, 1, 0], [0, 1, 1]],
-          ]], z.eval())
+
+  def test_meshgrid_3d(self):
+    x = util.meshgrid_3d([0, 0, 0], [1, 2, 2])
+
+    with self.test_session():
+      self.assertAllEqual([[[[0, 0, 0], [0, 0, 1]],
+                            [[0, 1, 0], [0, 1, 1]]]], x.eval())
+
+  def test_spatial_gradient_3d(self):
+    volume = np.random.randint(0, 10, [5, 10, 10, 10, 3])
+    gradx = volume[:, :, :, 1:, :] - volume[:, :, :, :-1, :]
+    grady = volume[:, :, 1:, :, :] - volume[:, :, :-1, :, :]
+    gradz = volume[:, 1:, :, :, :] - volume[:, :-1, :, :, :]
+
+    with self.test_session():
+      dz, dy, dx = util.spatial_gradient_3d(volume)
+
+      self.assertAllEqual(gradz, dz.eval())
+      self.assertAllEqual(grady, dy.eval())
+      self.assertAllEqual(gradx, dx.eval())
 
 
 if __name__ == '__main__':
--- python/mrtoct/util.py
+++ python/mrtoct/util.py
@@ -1,43 +1,48 @@
 import tensorflow as tf
 
 
-def meshgrid(start, stop, delta=1):
-  with tf.name_scope('meshgrid'):
+def meshgrid_2d(start, stop, delta=1):
+  with tf.name_scope('meshgrid_2d'):
     start = tf.convert_to_tensor(start)
     stop = tf.convert_to_tensor(stop)
 
     shape1 = start.shape.as_list()
     shape2 = stop.shape.as_list()
 
-    if len(shape1) != 1 or shape1 != shape2:
-      raise ValueError('start and stop shape should have same rank 1 shape')
+    if shape1 != shape2 or len(shape1) != 1 or shape1[0] != 2:
+      raise ValueError(f'start and stop should have shape [2] not {shape1}')
 
-    ndim = shape1[0]
-
-    rows = tf.range(start[-2], stop[-2], delta, dtype=tf.int32)
     cols = tf.range(start[-1], stop[-1], delta, dtype=tf.int32)
+    rows = tf.range(start[-2], stop[-2], delta, dtype=tf.int32)
 
-    if ndim == 2:
-      return tf.stack(tf.meshgrid(cols, rows, indexing='ij'), 2)
+    return tf.stack(tf.meshgrid(cols, rows, indexing='ij'), 2)
 
+
+def meshgrid_3d(start, stop, delta=1):
+  with tf.name_scope('meshgrid_3d'):
+    start = tf.convert_to_tensor(start)
+    stop = tf.convert_to_tensor(stop)
+
+    shape1 = start.shape.as_list()
+    shape2 = stop.shape.as_list()
+
+    if shape1 != shape2 or len(shape1) != 1 or shape1[0] != 3:
+      raise ValueError(f'start and stop should have shape [3] not {shape1}')
+
+    cols = tf.range(start[-1], stop[-1], delta, dtype=tf.int32)
+    rows = tf.range(start[-2], stop[-2], delta, dtype=tf.int32)
     slices = tf.range(start[-3], stop[-3], delta, dtype=tf.int32)
 
-    if ndim == 3:
-      return tf.stack(tf.meshgrid(slices, cols, rows, indexing='ij'), 3)
-
-    raise ValueError('start and stop shape dimension need to be 2 or 3')
+    return tf.stack(tf.meshgrid(slices, cols, rows, indexing='ij'), 3)
 
 
-def gradient(x):
-  ndims = x.get_shape().ndims
-  diffs = []
+def spatial_gradient_3d(volume):
+  with tf.name_scope('spatial_gradient_3d'):
+    volume = tf.convert_to_tensor(volume)
 
-  if ndims == 4:
-    diffs.append(tf.abs(x[:, 1:, :, :] - x[:, :-1, :, :]))
-    diffs.append(tf.abs(x[:, :, 1:, :] - x[:, :, :-1, :]))
-  if ndims == 5:
-    diffs.append(tf.abs(x[:, 1:, :, :, :] - x[:, :-1, :, :, :]))
-    diffs.append(tf.abs(x[:, :, 1:, :, :] - x[:, :, :-1, :, :]))
-    diffs.append(tf.abs(x[:, :, :, 1:, :] - x[:, :, :, :-1, :]))
+    if volume.get_shape().ndims != 5:
+      raise ValueError(f'volume should have rank 5 not {ndims}')
 
-  return diffs
+    return [volume[:, 1:, :, :, :] - volume[:, :-1, :, :, :],
+            volume[:, :, 1:, :, :] - volume[:, :, :-1, :, :],
+            volume[:, :, :, 1:, :] - volume[:, :, :, :-1, :]]
