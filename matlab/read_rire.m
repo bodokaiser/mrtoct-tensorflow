86523f4565cb065700e57ec069f23fcfade6eb05
Bodo Kaiser <i@bodokaiser.io>
2018-03-02 14:11:49 +0100

    removed matlab scripts, added evaluation notebooks

--- matlab/read_rire.m
+++
@@ -1,4 +0,0 @@
-function [header, volume] = read_rire(headerpath, volumepath)
-header = read_rire_header(headerpath);
-volume = read_rire_volume(volumepath, header);
-end
--- matlab/read_rire_volume.m
+++
@@ -1,5 +0,0 @@
-function volume = read_rire_volume(filename, header)
-volume = multibandread(filename, [header.Rows, header.Columns, ...
-    header.Slices], 'int16', 0, 'bsq', 'ieee-be');
-end
-
